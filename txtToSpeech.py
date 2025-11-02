fbbc9b1d76f91e20897d49a169d79351dee5aa9f
ButtiHari <haributti22@gmail.com>
2025-11-02 17:09:00 +0530

    Update txtToSpeech.py

--- txtToSpeech.py
+++ txtToSpeech.py
@@ -1,6 +1,6 @@
 from gtts import gTTS 
 import os
-file = open("abc.txt", "r").read()
+file = open("text.txt", "r").read()
 
 speech = gTTS(text=file, lang='en', slow=False)
 speech.save("voice.mp3")
